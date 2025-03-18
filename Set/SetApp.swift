7cb90c85818d2b617556e7e23884b4e52fe8588e
fromo0214 <faromo0214@gmail.com>
2025-03-18 14:16:45 -0700

    Initial Commit

---
+++ Set/SetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetApp.swift
+//  Set
+//
+//  Created by Fernando Romo on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct SetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
